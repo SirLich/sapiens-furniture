f5e4fe003d26deb5416425c346922b2b717905dc
SirLich <lrkoehler77@gmail.com>
2023-04-19 22:48:48 +0200

    Initial push

---
+++ localizations/en_us/localizations.lua
@@ -0,0 +1,13 @@
+local localizations = {}
+
+localizations.values = {
+    -- OBJECTS
+    object_woodStock = "Wood Stock",
+    object_woodStock_plural = "Wood Stock",
+    object_woodStock_summary = "Refined wood parts. Used to build furniture.",
+
+    -- STORAGE AREAS
+    storage_woodStock_storage = "Wood Stock",
+}
+
+return localizations
---
+++ modInfo.lua
@@ -0,0 +1,10 @@
+local modInfo = {
+    name = "Furniture",
+    description = "An expansion mod for Sapiens, introducing a collection of chairs and tables.",
+    version = "1.0.0",
+    type = "world",
+    developer = "SirLich",
+    loadOrder = 1
+}
+
+return modInfo
