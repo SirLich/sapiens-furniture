8bd6724915e8f851b2e50c47b308789bedfd28c2
SirLich <lrkoehler77@gmail.com>
2023-04-19 23:19:33 +0200

    Add wood stock variations

--- localizations/en_us/localizations.lua
+++ localizations/en_us/localizations.lua
@@ -1,10 +1,17 @@
 local localizations = {}
 
 localizations.values = {
-    -- OBJECTS
-    object_woodStock = "Wood Stock",
-    object_woodStock_plural = "Wood Stock",
-    object_woodStock_summary = "Refined wood parts. Used to build furniture.",
+    -- WOOD STOCK
+    woodStock_summary = "Refined wood parts. Used to build furniture.",
+
+    woodStock_aspen = "Aspen Wood Stock",
+    woodStock_birch = "Birch Wood Stock",
+    woodStock_bamboo = "Bamboo Wood Stock",
+    woodStock_orange = "Orange Wood Stock",
+    woodStock_peach= "Peach Wood Stock",
+    woodStock_elderberry= "Elderberry Wood Stock",
+    woodStock_line= "Pine Wood Stock",
+    woodStock_willow= "Willow Wood Stock",
 
     -- STORAGE AREAS
     storage_woodStock_storage = "Wood Stock",
