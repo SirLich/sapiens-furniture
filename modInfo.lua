db850f7f42c70c79fc929fe39eb282625bbe7b59
SirLich <lrkoehler77@gmail.com>
2023-04-23 16:13:00 +0200

    Update version

--- modInfo.lua
+++ modInfo.lua
@@ -1,6 +1,6 @@
 local modInfo = {
     name = "Furniture",
-    description = "An expansion mod for Sapiens, introducing a collection of chairs and tables.",
+    description = "An expansion mod for Sapiens, introducing a of wooden chairs. More to come!",
     version = "1.0.0",
     type = "world",
     developer = "SirLich",
