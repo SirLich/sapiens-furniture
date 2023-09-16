ae7e3cc4238ca5918b5091a03fab51afe7fb8d87
SirLich <lrkoehler77@gmail.com>
2023-09-16 11:43:39 +0200

    Add cloth recipe

---
+++ hammerstone/objects/cloth.recipe.lua
@@ -0,0 +1,35 @@
+return {
+	description = {
+		identifier = "brown_cloth", -- Sort of a bug work-around. We need a valid game-object identifier to harness the recipe to.
+		name = "Cloth",
+		plural = "Cloth",
+		description = "A refined cloth material, which can be used for furthe crafting."
+	},
+	components = {
+		hs_craftable = {
+			skill = "butchery",
+			research = "butchery",
+			tool = "butcher",
+
+			action_sequence = "butcher",
+
+			hs_output = {
+				simple_output = {
+					"brown_cloth",
+					"brown_cloth"
+				}
+			},
+
+			resources = {
+				{
+					resource = "woolskin",
+					action = {
+						action_type = "inspect",
+						duration = 0.4,
+						duration_without_skill = 15
+					}
+				}
+			}
+		}
+	}
+}
